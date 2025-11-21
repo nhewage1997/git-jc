0d1aad286acc65af24982fbd25021d23f6d04bef
George Funkenbusch <gtf3@duke.edu>
2025-11-21 13:22:15 -0500

    Fixed triangle volume test

--- test.py
+++ test.py
@@ -72,7 +72,7 @@ class TestTriangle(unittest.TestCase):
     def test_triangle_volume(self):
         t = Triangle(3, 4, 5)
         height = 10
-        base_area = 6.0
+        base_area = 3.0
         expected = base_area * height
         self.assertAlmostEqual(t.volume(height=height), expected, places=5)
 
